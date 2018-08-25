6db1dfd4093c5f4a81eb68a7387547e9a4bdf114
Max <genmax100.ab@gmail.com>
2018-08-25 14:27:56 -0700

    Heroku

--- config/apollo.js
+++ config/apollo.js
@@ -1,6 +1,6 @@
 
-require('cross-fetch/polyfill');
-const ApolloClient = require('apollo-boost');
+import 'cross-fetch/polyfill';
+import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
   uri: 'https://api.graph.cool/simple/v1/cjksyk40v80sg0145j58iwet4'
