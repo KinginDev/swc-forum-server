988d568b05e188bb3eef98e823135abadddc28c3
Max <genmax100.ab@gmail.com>
2018-08-25 14:08:17 -0700

    Fixed the Heroku error

--- config/apollo.js
+++ config/apollo.js
@@ -1,6 +1,6 @@
 
-import 'cross-fetch/polyfill';
-import ApolloClient from 'apollo-boost';
+require('cross-fetch/polyfill');
+const ApolloClient = require('apollo-boost');
 
 const client = new ApolloClient({
   uri: 'https://api.graph.cool/simple/v1/cjksyk40v80sg0145j58iwet4'
